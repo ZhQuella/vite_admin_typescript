db1dc5e810007f6adb0b14c546c7873ad0aaf4db
ZhQuella <827265905@qq.com>
2021-12-21 22:45:52 +0800

    feat(add) '添加eslit自动保存代码功能'

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>Home</div>
+  <div>
+    Home
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
+  name: "HomePage",
   setup() {
     return {};
   },
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    eslintPlugin({
+      cache: false,
+      exclude: ['script/**/*.js']
+    }),
+    vue()
+  ],
+  css: {
+		preprocessorOptions: {
+			scss: {
+        /*
+				引入var.scss全局预定义变量，
+				如果引入多个文件，
+				可以使用
+				'@import "@/assets/scss/globalVariable1.scss";
+        @import "@/assets/scss/globalVariable2.scss";'
+				这种格式
+				*/
+			}
+		}
+	},
   resolve: {
     alias: [
       {
